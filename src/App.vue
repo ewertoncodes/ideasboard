d74de8036df1e9186a0e7e6e572b49bed8e26299
ewertonorg <ewertonorg@gmail.com>
2019-07-21 21:33:46 -0300

    create ideas component

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Ideas/>
+  </div>
+</template>
+
+<script>
+import Ideas from './components/Ideas'
+
+export default {
+  name: 'app',
+  components:{
+    Ideas
+  }
+  ,
+  data () {
+    return {
+     ideas:[]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Ideas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="idea in ideas" :key="idea.id">
+        <h1>{{idea.title}}</h1>
+        <p>{{idea.body}}</p> 
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      ideas:[]
+    }
+  },
+  created(){
+    //onsole.log(this.ideas)
+    this.$http.get('http://localhost:3001/api/v1/ideas')
+      .then(response => response.json())
+      .then(ideas => this.ideas = ideas)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
